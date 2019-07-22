6038eff676952e29cce484428faf0e06e21a69f2
arsandev <tganteng77@gmail.com>
2019-07-22 15:23:07 +0700

    add nprogress and dynamic title for component

--- src/router.js
+++ src/router.js
@@ -1,25 +1,39 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
+
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const PanelRouter = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
+      meta:{title:'Welcome'},
       component: Home
     },
     {
       path: '/about',
+      meta:{title:'About me'},
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     }
   ]
 })
+
+PanelRouter.beforeEach((to, from, next) => {
+  document.title = to.meta.title // Set <title> for every components
+  NProgress.start()
+  NProgress.set(0.5)
+  next()
+})
+PanelRouter.afterEach(() => {
+  setTimeout(() => NProgress.done(), 20)
+})
+
+export default PanelRouter
