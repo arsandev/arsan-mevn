fcfe5291aec7540096f7e6783cf028cda74483bc
Arsan Dev <tganteng@gmail.com>
2019-10-21 15:35:35 +0700

    c

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <form method="post" onsubmit="return false">
+      <input type="text" autocomplete="off" ref="name">
+      <input type="submit" @click.prevent="submit()">
+    </form>
   </div>
 </template>
+
+<script>
+  export default{
+    methods: {
+      submit(){
+        this.axios.post('api/test',{
+          name:this.$refs.name.value
+        })
+      }
+    }
+  }
+</script>
