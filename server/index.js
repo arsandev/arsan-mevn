765396cc70d7e8511cc6ca2df46645cfcf995490
Arsan Dev <tganteng@gmail.com>
2019-10-24 10:21:28 +0700

    change server structure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import helmet from 'helmet'
 import fs from 'fs'
 import path from 'path'
+import tokenRoute from './routers/_JWT'
 import 'dotenv/config'
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cookieParser())
 app.use(cors())
 app.use(helmet())
 
+app.all('*', tokenRoute)
 fs.readdirSync(path.join(__dirname, 'routers')).forEach(file => {
   let router = file.split('Router.js')
   app.use('/api/'+router[0], require('./routers/'+file))
@@ -21,7 +25,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req,res)=>{ res.sendFile(__dirname+'/vue/index.html') })
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || process.env.WEB_PORT
 app.listen(port,()=>{
   console.log('listened on '+port)
 })
