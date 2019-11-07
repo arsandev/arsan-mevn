4378fe66074597c5ff8dc0b478fa96294c7659d0
Arsan Dev <tganteng@gmail.com>
2019-11-07 22:56:42 +0700

    separating db config to env

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const urlConfig = process.env.DB_URL
-mongoose.connect(urlConfig, {useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true}, (err,conn)=>{
+mongoose.connect(urlConfig, {useNewUrlParser:process.env.useNewUrlParser,useUnifiedTopology:process.env.useUnifiedTopology,useFindAndModify:process.env.useFindAndModify,useCreateIndex:process.env.useCreateIndex}, (err,conn)=>{
   if (err) console.log(err)
   else console.log('db connected')
 })
