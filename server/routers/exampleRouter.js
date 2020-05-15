d4137ce70b9e3148d735222a811a854d4630d283
ARSAN <arsandev@users.noreply.github.com>
2020-05-15 22:22:39 +0700

    update bug router

--- server/routers/exampleRouter.js
+++ server/routers/exampleRouter.js
@@ -3,21 +3,21 @@ const router = express.Router()
 import m from "../models/example"
 import jwt from "../library/JWT"
 
-router.get("/", (req,res)=>{ // Get All
-  m.find({}, (err,data)=>{
+router.get("/", (req,res) => { // Get All
+  m.find({}, (err,data) => {
     if (err) res.json({status:false,msg:err})
     else res.json({status:true,data:data})
   })
 })
 
-router.get("/:id", (req,res)=>{ // Find One By Id
-  m.findOne({_id:req.params.id}, (err,data)=>{
+router.get("/:id", (req,res) => { // Find One By Id
+  m.findOne({_id:req.params.id}, (err,data) => {
     if (err) res.json({status:false,msg:err})
     else res.json({status:true,data:data})
   })
 })
 
-router.post('/", (req,res)=>{ // Post Into Model
+router.post("/", (req,res) => { // Post Into Model
   m.create({
     // Body Post
   }, (err,succ)=>{
@@ -26,7 +26,7 @@ router.post('/", (req,res)=>{ // Post Into Model
   })
 })
 
-router.put("/update/:id", (req,res)=>{ // Update One By Id
+router.put("/update/:id", (req,res) => { // Update One By Id
   m.findOneAndUpdate({_id:req.params.id}, {$set:{
     // Update Body Post
   }}, (err,succ)=>{
@@ -35,8 +35,8 @@ router.put("/update/:id", (req,res)=>{ // Update One By Id
   })
 })
 
-router.delete("/delete/:id", (req,res)=>{ // Delete One By Id
-  m.remove({_id:req.params.id}, (err,succ)=>{
+router.delete("/delete/:id", (req,res) => { // Delete One By Id
+  m.remove({_id:req.params.id}, (err,succ) => {
     if (err) res.json({status:false,msg:err})
     else res.json({status:true,msg:'success'})
   })
